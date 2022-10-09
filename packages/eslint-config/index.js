a6c4226f43a8f056aaf0e64e2c233552a8133095
gregory6z <gregoryrag@gmail.com>
2022-10-09 17:15:38 +0200

    add eslint  ✔️

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@rocketseat/eslint-config/react'],
+  rules: {
+    "prettier/prettier": ["error", {
+      "printWidth": 80,
+      "tabWidth": 2,
+      "singleQuote": false,
+      "trailingComma": "all",
+      "arrowParens": "always",
+      "semi": false
+    }]
+  }
+}
